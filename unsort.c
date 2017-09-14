50ffb88e94504b6efeb7728d68d0f8e1a95b4d4e
MonishaMuthu96 <31921307+MonishaMuthu96@users.noreply.github.com>
2017-09-14 19:20:53 +0530

    Add files via upload

---
+++ unsort.c
@@ -0,0 +1,22 @@
+#include<stdio.h> 
+int main(void) 
+{ 
+int j,a[100],i,n,flag; 
+scanf("%d",&n); 
+for(i=0;i<n;i++) 
+{ 
+flag=0; 
+for(j=0;j<n;j++) 
+{ 
+if(a[i]== a[j]) 
+{ 
+flag++; 
+} 
+else 
+continue; 
+} 
+if(flag==1) 
+printf("%d",a[i]); 
+} 
+return 0; 
+} 
