09a84d95cfc0421225538fa58f4ac09fdcc27496
MonishaMuthu96 <31921307+MonishaMuthu96@users.noreply.github.com>
2017-09-14 22:00:20 +0530

    Add files via upload

---
+++ Co_prime.java
@@ -0,0 +1,28 @@
+import java.util.Scanner; 
+public class Co_prime {  
+public static void main(String[] args) { 
+Scanner in=new Scanner(System.in); 
+System.out.println("ENTER THE TWO VALUES"); 
+int x=in.nextInt(); 
+int y=in.nextInt(); 
+int z=iscoprime(x,y); 
+if(z==1) 
+System.out.println("GIVEN NUMBERS ARE CO-PRIME"); 
+else 
+System.out.println("GIVEN NUMBERS ARE NOT CO-PRIME"); 
+15 
+}
+public static int iscoprime(int a,int b) 
+{ 
+while(1>0) 
+{ 
+int temp=a%b; 
+if(temp==0) 
+{ 
+return b; 
+} 
+a=b; 
+b=temp; 
+}
+}
+}
