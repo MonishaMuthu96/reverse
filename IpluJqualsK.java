2b42072d83393044d73a12b4b5add8181bae6bba
MonishaMuthu96 <31921307+MonishaMuthu96@users.noreply.github.com>
2017-09-14 19:25:58 +0530

    Add files via upload

---
+++ IpluJqualsK.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.net.*; 
+class IpluJqualsK 
+{ 
+public static void main(String args[]) 
+{ 
+Scanner sc=new Scanner(System.in); 
+int n=sc.nextInt(),arr[]=new int[n]; 
+String s=""; 
+for(int i=0;i<n;i++) 
+arr[i]=sc.nextInt(); 
+for(int i=0;i<n;i++) 
+for(int j=0;j<i;j++) 
+for(int k=i;k<n;k++) 
+if(arr[i]+arr[j]==arr[k]) 
+s=s+arr[j]+" + "+arr[i]+" = "+arr[k]+"\n"; 
+System.out.println((s=="")?"No numbers were found":s); 
+} 
+} 
