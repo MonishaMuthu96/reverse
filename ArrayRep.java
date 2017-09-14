495802d6999d7c8020c2257d32cc03e3576ba75e
MonishaMuthu96 <31921307+MonishaMuthu96@users.noreply.github.com>
2017-09-14 21:14:27 +0530

    Add files via upload

---
+++ ArrayRep.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList; 
+import java.util.List; 
+import java.util.Scanner; 
+import java.io.*; 
+public class ArrayRep { 
+public static void main(String[] args) {  
+Scanner sc=new Scanner(System.in); 
+int k; 
+System.out.println("Enter the number of arrays"); 
+k=sc.nextInt(); 
+int v=0,tempi,tempj,temp,count=0; 
+System.out.println("Enter the size of arrays"); 
+int n=sc.nextInt(); 
+List<Integer> a[]=(List<Integer>[])new ArrayList[k]; 
+for(int i=0;i<k;i++) 
+{ 
+a[i]=new ArrayList<>(); 
+for(int j=0;j<n;j++) 
+{
+a[i].add(j,sc.nextInt()); 
+}
+}
+for(int i=0;i<k;i++) 
+{
+for(int j=0;j<n;j++) 
+{		 
+temp=a[i].get(j); 
+count=0; 
+for(int l=0;l<k;l++) 
+{	 
+if(a[l].contains(temp)) 
+{ 
+count++; 
+} 
+} 
+if(count>1) 
+{ 
+System.out.println(temp+" is repeating"); 
+}
+}
+}
+}
+}
