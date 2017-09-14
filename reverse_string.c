1ccd774f97c04e6f71d2c57cac35eae9440ed256
MonishaMuthu96 <31921307+MonishaMuthu96@users.noreply.github.com>
2017-09-14 20:52:04 +0530

    Add files via upload

---
+++ reverse_string.c
@@ -0,0 +1,12 @@
+#include<stdio.h> 
+#include<conio.h> 
+void main() 
+{ 
+char c; 
+clrscr(); 
+printf("Enter a word: "); 
+scanf("%s",c); 
+strrev(c); 
+printf("Reverse string is %s",c); 
+ getch(); 
+} 
